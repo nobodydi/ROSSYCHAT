10320b6384a28c98a6caedc0bc254fe6d0b0d175
nobodydi <149184961+nobodydi@users.noreply.github.com>
2024-01-25 00:46:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "6435225"
+# -------------------------------------------------------------
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = "6358262816"
+SUPPORT_GRP = "BABYBOTSX"
+UPDATE_CHNL = "BABYUPDATE"
+OWNER_USERNAME = "VM_COOL"
+
